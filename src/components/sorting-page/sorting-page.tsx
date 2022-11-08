a2ca8c5da907cfadfc2a4603737a85fcfb864d6a
pankiale <93738391+pankiale@users.noreply.github.com>
2022-11-08 19:17:29 +0100

    Update sorting-page.tsx

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const SortingPage: React.FC = () => {
+
+  const random = (minLen: number = 3, maxLen: number = 17): number[] => {
+    let arr: number[] = [];
+    let arrLength = Math.floor((Math.random())*(maxLen-minLen+1))+minLen;
+    arrLength
+    let n = 0
+    while (n < arrLength) {
+      arr.push(Math.floor(Math.random()*101));
+      n ++
+    };
+    return arr;
+  }
+
+
   return (
     <SolutionLayout title="Сортировка массива">
 
